5f1b8659dfc6f7b6b7d7c055820a5aeb09fd9e42
molchoun <ne.molchoun@gmail.com>
2023-08-31 19:23:36 +0400

    add app settings

--- greeting_api/urls.py
+++ greeting_api/urls.py
@@ -1,10 +1,10 @@
 """
 URLs for greeting_api.
 """
-from django.urls import re_path  # pylint: disable=unused-import
+from django.urls import re_path, path  # pylint: disable=unused-import
 from greeting_api import views# pylint: disable=unused-import
 
 urlpatterns = [
     # TODO: Fill in URL patterns and views here.
-    re_path(r'greeting/', views.GreetingView.as_view()),
+    path(r'greeting/', views.GreetingView.as_view()),
 ]
