a41ebc4d794d338f482e378c39839587f1aa70c5
molchoun <ne.molchoun@gmail.com>
2023-09-18 20:47:10 +0400

    add authentication

--- greeting_api/models.py
+++ greeting_api/models.py
@@ -1,12 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 
 class Greeting(models.Model):
     message = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        """
-        Get a string representation of this model instance.
-        """
-        # TODO: return a string appropriate for the data fields
-        return self.message
+        return f"Message from user: {self.message}"
--- greeting_api/views.py
+++ greeting_api/views.py
@@ -1,29 +1,41 @@
+import logging
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from greeting_api.serializers import GreetingSerializer
 from django.contrib.auth import get_user_model
-from openedx.core.lib.api.view_utils import view_auth_classes
+from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
+from edx_rest_framework_extensions.auth.session.authentication import SessionAuthenticationAllowInactiveUser
+from openedx.core.lib.api.authentication import BearerAuthenticationAllowInactiveUser
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from greeting_api.models import Greeting
 
 
-@view_auth_classes(is_authenticated=True)
+logger = logging.getLogger(__name__)
+
+
 class GreetingView(APIView):
-
+    authentication_classes = (
+        JwtAuthentication,
+        BearerAuthenticationAllowInactiveUser,
+        SessionAuthenticationAllowInactiveUser,
+    )
+    permission_classes = (IsAuthenticated, IsAuthenticatedOrReadOnly)
     def post(self, request):
+
         serializer = GreetingSerializer(data=request.data)
-        username = request.user.username
         if serializer.is_valid():
             message = serializer.data["message"]
-
+            logger.info(f"Message from user: {message}")
+            Greeting.objects.create(message=message)
             if message.lower() == "hello":
                 message = "goodbye"
                 request.data._mutable = True
                 request.data["message"] = message
                 self.post(request)
-                return Response({"user": username, "text": message})
 
         message = serializer.data["message"]
-        return Response({"user": username, "text": message})
+        return Response({"Message from user": message})
 
     def get(self, request):
         return Response({"message": "Hello!"})
