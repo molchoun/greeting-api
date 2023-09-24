6c6163ddb0895059445138c960a4905651d7f04e
molchoun <ne.molchoun@gmail.com>
2023-09-24 12:55:52 +0400

    add authentication classes

--- greeting_api/urls.py
+++ greeting_api/urls.py
@@ -5,6 +5,5 @@ from django.urls import re_path, path  # pylint: disable=unused-import
 from greeting_api import views# pylint: disable=unused-import
 
 urlpatterns = [
-    # TODO: Fill in URL patterns and views here.
     path('greeting/', views.GreetingView.as_view()),
 ]
